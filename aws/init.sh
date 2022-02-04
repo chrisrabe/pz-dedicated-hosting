ccb81490e9daa06e2a3833e6bd04525b65baafc4
Chris Rabe <chris.rabe14@gmail.com>
2022-02-04 17:44:32 +1000

    feat: automatically open firewalls

--- aws/init.sh
+++ aws/init.sh
@@ -9,3 +9,8 @@ sudo snap disable docker
 sudo snap enable docker
 
 # Set up firewalls
+sudo ufw enable
+sudo ufw allow 22/tcp
+sudo ufw allow 8766/udp
+sudo ufw allow 16261/udp
+sudo ufw allow 16262:16272/tcp
