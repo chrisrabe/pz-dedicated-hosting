8a083ff45d1ddd945e9cbb6229eaa27c13725e47
Chris Rabe <chris.rabe14@gmail.com>
2022-02-05 13:23:56 +1000

    fix: export discord webhook to bash profile

--- aws/user-data.sh
+++ aws/user-data.sh
@@ -7,7 +7,7 @@ SETUP_DIR=${USER_HOME}/server-setup
 STEAM_APP_ID=380870
 AWS_DIR=${SCRIPTS_DIR}/aws
 ASSETS_DIR=${SCRIPTS_DIR}/assets
-export DISCORD_WEBHOOK=''
+DISCORD_WEBHOOK=''
 
 # Install Docker Engine
 sudo snap install docker
@@ -33,6 +33,10 @@ git clone ${START_SCRIPTS} ${SCRIPTS_DIR}
 
 chown -R ubuntu:ubuntu ${SETUP_DIR}
 chown -R ubuntu:ubuntu ${SCRIPTS_DIR}
+
+# Insert discord webhook in .bashrc (so it's available globally)
+echo "export DISCORD_WEBHOOK=${DISCORD_WEBHOOK}" >> ${USER_HOME}/.bashrc
+source ${USER_HOME}/.bashrc
 
 # Send server initialisation message to discord
 if [ ! -z "${DISCORD_WEBHOOK}" ];
