28dc8649ecb7f6081d7b9c38dec72259178e737e
Chris Rabe <chris.rabe14@gmail.com>
2022-02-19 17:53:20 +1000

    fix: added fifo input

--- aws/run-server.sh
+++ aws/run-server.sh
@@ -6,13 +6,11 @@ ASSETS=${SCRIPTS_DIR}/assets
 SETUP_DIR=${USER_HOME}/server-setup
 SERVER_INPUT_PIPE=/tmp/srv-input
 
-rm ${SERVER_INPUT_PIPE}
+rm -f ${SERVER_INPUT_PIPE}
 mkfifo ${SERVER_INPUT_PIPE}
 
 cat > ${SERVER_INPUT_PIPE} &
 echo $! > ${SERVER_INPUT_PIPE}-pid
-echo password >> ${SERVER_INPUT_PIPE}
-echo password >> ${SERVER_INPUT_PIPE}
 
 # Send server startup message to Discord
 if [ ! -z "${DISCORD_WEBHOOK}" ];
@@ -21,16 +19,17 @@ then
     curl -H "Content-Type: application/json" -d "{\"content\": \"Server at ${SERVER_IP} is starting. Please wait a few minutes before joining!\"}" "${DISCORD_WEBHOOK}"
 fi
 
-if [ -d "${USER_HOME}/Zomboid" ];
-then
-    ${SETUP_DIR}/start-server.sh
-else
-    ${SETUP_DIR}/start-server.sh < ${SERVER_INPUT_PIPE}
-fi
+${SETUP_DIR}/start-server.sh < ${SERVER_INPUT_PIPE} &
+
+sleep 15s
+echo "password" > ${SERVER_INPUT_PIPE}
+sleep 1s
+echo "password" > ${SERVER_INPUT_PIPE}
 
 # Send server exit to Discord
 if [ ! -z "${DISCORD_WEBHOOK}" ];
 then
+    sleep 30s
     SERVER_IP=$(curl http://checkip.amazonaws.com)
     curl -H "Content-Type: application/json" -d "{\"content\": \"Server at ${SERVER_IP} is closed.\"}" "${DISCORD_WEBHOOK}"
 fi
