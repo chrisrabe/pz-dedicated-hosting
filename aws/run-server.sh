90fb6bfced3d581b88fc4fe0951f0d53f6d29dde
Chris Rabe <chris.rabe14@gmail.com>
2022-02-19 17:26:48 +1000

    feat: modified run script to have fifo pipes

--- aws/run-server.sh
+++ aws/run-server.sh
@@ -4,7 +4,15 @@ USER_HOME=/home/ubuntu
 SCRIPTS_DIR=${USER_HOME}/pz-setup
 ASSETS=${SCRIPTS_DIR}/assets
 SETUP_DIR=${USER_HOME}/server-setup
+SERVER_INPUT_PIPE=/tmp/srv-input
 
+rm ${SERVER_INPUT_PIPE}
+mkfifo ${SERVER_INPUT_PIPE}
+
+cat > ${SERVER_INPUT_PIPE} &
+echo $! > ${SERVER_INPUT_PIPE}-pid
+echo password >> ${SERVER_INPUT_PIPE}
+echo password >> ${SERVER_INPUT_PIPE}
 
 # Send server startup message to Discord
 if [ ! -z "${DISCORD_WEBHOOK}" ];
@@ -17,7 +25,7 @@ if [ -d "${USER_HOME}/Zomboid" ];
 then
     ${SETUP_DIR}/start-server.sh
 else
-    ${SETUP_DIR}/start-server.sh < ${ASSETS}/serv-creds.txt
+    ${SETUP_DIR}/start-server.sh < ${SERVER_INPUT_PIPE}
 fi
 
 # Send server exit to Discord
